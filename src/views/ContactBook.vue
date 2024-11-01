99c9734056b9df3bd658a3f8d23be3ef8e3d508c
Chau Trong Phuc <fut.chautrongphuc@gmail.com>
2024-11-01 19:15:38 +0700

    Tao trang cap nhat lien he

--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -36,6 +36,14 @@
                     <i class="fas fa-address-card"></i>
                 </h4>
                 <ContactCard :contact="activeContact"/>
+                <router-link
+                    :to="{
+                        name: 'contact.edit',
+                        params: { id: activeContact._id },
+                    }"
+                >
+                <span class="mt-2 badge badge-danger"><i class="fas fa-edit"></i> Hiệu chỉnh</span>
+                </router-link>
             </div>
         </div>
     </div>
@@ -126,5 +134,10 @@
     .page {
         text-align: left;
         max-width: 750px;
+    }
+    .badge-danger {
+        opacity: 1 !important;
+        color: #fff;
+        background-color: #dc3545;
     }
 </style>
